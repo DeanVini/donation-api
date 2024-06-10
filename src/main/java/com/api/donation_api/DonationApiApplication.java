82f620080d2ef1b29c029ff7ed2fd73b558b368c
DeanVini <deanvinicius2003@gmail.com>
2024-06-09 21:19:04 -0300

    feat: configuração inicial

---
+++ src/main/java/com/api/donation_api/DonationApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.donation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationApiApplication.class, args);
+	}
+
+}
