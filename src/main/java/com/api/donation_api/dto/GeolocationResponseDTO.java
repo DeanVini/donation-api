2c97b7ea85346957be456bd0179eb9df5c523651
DeanVini <deanvinicius2003@gmail.com>
2025-03-09 02:28:50 -0300

    feat: Service to extract Geolocation infos from an Address

--- src/main/java/com/api/donation_api/dto/GeolocationResponseDTO.java
+++ src/main/java/com/api/donation_api/dto/GeolocationResponseDTO.java
@@ -2,18 +2,20 @@ package com.api.donation_api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GeolocationResponseDTO {
-
     @JsonProperty("lat")
     private double latitude;
 
     @JsonProperty("lon")
     private double longitude;
-
-    @JsonProperty("display_name")
-    private String displayName;
 }
