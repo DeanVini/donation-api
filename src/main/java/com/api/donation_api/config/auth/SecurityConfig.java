7151e18db4611b035d8af90407908d1b5af73622
DeanVini <deanvinicius2003@gmail.com>
2025-03-09 13:51:52 -0300

    hotfix: correct allowedOrigins

--- src/main/java/com/api/donation_api/config/auth/SecurityConfig.java
+++ src/main/java/com/api/donation_api/config/auth/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         http.cors(cors -> cors
                         .configurationSource(request -> {
                             CorsConfiguration config = new CorsConfiguration();
-                            config.setAllowedOrigins(List.of("allowedOrigins"));
+                            config.setAllowedOrigins(List.of(allowedOrigins));
                             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                             config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                             config.setAllowCredentials(true);
